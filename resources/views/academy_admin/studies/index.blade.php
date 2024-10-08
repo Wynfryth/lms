cf5bcea09a864fee5ba34bb4bfa638b72b88ffc9
Bonifacius Sarumpaet <59567439+Wynfryth@users.noreply.github.com>
2024-10-09 01:11:29 +0700

    Dark Mode Optimization
    Pada td tabel edie detail studies

--- resources/views/academy_admin/studies/index.blade.php
+++ resources/views/academy_admin/studies/index.blade.php
@@ -50,9 +50,9 @@
                             @forelse ($studies as $index => $item)
                                 @php
                                     if($item->is_active == 1){
-                                        $row_color = 'bg-white';
+                                        $row_color = 'bg-white dark:bg-gray-800';
                                     }else{
-                                        $row_color = 'bg-zinc-300';
+                                        $row_color = 'bg-zinc-300 dark:bg-white';
                                     }
                                 @endphp
                                 <tr>
